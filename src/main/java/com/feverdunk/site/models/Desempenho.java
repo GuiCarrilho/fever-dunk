85eb487852d7c270714a1d132c2a18fe9a330a7e
Celio Camara <celiofcjunior_cg@hotmail.com>
2023-11-03 17:40:50 -0300

    create jogador and desempenho

---
+++ src/main/java/com/feverdunk/site/models/Desempenho.java
@@ -0,0 +1,40 @@
+package com.feverdunk.site.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "desempenhos")
+public class Desempenho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data")
+    @NotNull
+    private LocalDate data;
+
+    @Column(name = "pontos")
+    @NotNull
+    private int pontos;
+
+    @Column(name = "rebotes")
+    @NotNull
+    private int rebotes;
+
+    @Column(name = "assisntencias")
+    @NotNull
+    private int assistencias;
+
+    @Column(name = "min_jogados")
+    @NotNull
+    private int minJogados;
+
+    @ManyToOne
+    @JoinColumn(name = "jogador_id")
+    private Jogador jogador;
+
+}
---
+++ src/main/java/com/feverdunk/site/models/Jogador.java
@@ -0,0 +1,49 @@
+package com.feverdunk.site.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name = "jogadores")
+public class Jogador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    @NotBlank
+    private String nome;
+
+    @Column(name = "posicao")
+    @NotBlank
+    private String posicao;
+
+    @Column(name = "altura")
+    @NotNull
+    private int altura;
+
+    @Column(name = "idade")
+    @NotNull
+    private int idade;
+
+    @Column(name = "valor")
+    @NotNull
+    private int valor;
+
+    @Column(name = "time_real")
+    @NotBlank
+    private String timeReal;
+
+    @Column(name = "pontuacao")
+    @NotNull
+    private int pontuacao;
+
+    @OneToMany(mappedBy = "jogador")
+    private List<Desempenho> desempenhos;
+
+}
+
+
