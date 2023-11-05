6ad739efb2ecc88b87a42a88d890bafd30400174
GuiCarrilho <guicarrilho2001@gmail.com>
2023-11-05 00:02:12 -0300

    create contrato

---
+++ src/main/java/com/feverdunk/site/models/Contrato.java
@@ -0,0 +1,33 @@
+package com.feverdunk.site.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "contrato")
+public class Contrato {
+    @ManyToOne
+    @JoinColumn(name = "jogador_id")
+    private Jogador jogador;
+
+    @ManyToOne
+    @JoinColumn(name = "time_id")
+    private Time time;
+
+    @Column(name = "adquirido_em")
+    @NotNull
+    private LocalDateTime adquiridoEm;
+
+    @Column(name = "vendido_em")
+    private LocalDateTime vendidoEm;
+}
