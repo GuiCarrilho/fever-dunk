2ab8c59edc392faab4dd7f3206a0aae837a4c1b7
GabsGullo <gabrielgullo30@gmail.com>
2023-11-05 00:26:08 -0300

    Finished Put, Post and Delete for Manager

--- src/main/java/com/feverdunk/site/controller/ManagerController.java
+++ src/main/java/com/feverdunk/site/controller/ManagerController.java
@@ -1,11 +1,15 @@
 package com.feverdunk.site.controller;
 
+import com.feverdunk.site.exceptions.ObjectNotFoundException;
 import com.feverdunk.site.models.Manager;
 import com.feverdunk.site.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,5 +24,43 @@ public class ManagerController {
     @GetMapping
     public List<Manager> getManager(){
         return managerService.getManager();
+    }
+
+    @GetMapping("/{id}")
+    public Manager getById(Long id){
+        return managerService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Manager> post(@RequestBody @Validated Manager manager){
+        return criarMenager(manager);
+    }
+
+    @PutMapping
+    public ResponseEntity<Manager> put(@RequestBody @Validated Manager manager){
+        try{
+            managerService.findById(manager.getId());
+
+            Manager managerAtualizado = managerService.update(manager);
+            return ResponseEntity.ok(managerAtualizado);
+        }catch (ObjectNotFoundException e){
+            return criarMenager(manager);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Manager> delete(@PathVariable Long id){
+        managerService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    public ResponseEntity<Manager> criarMenager(@RequestBody @Validated Manager manager){
+        Manager managerCriado = managerService.create(manager);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/{id}").buildAndExpand(managerCriado.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/feverdunk/site/service/ManagerService.java
+++ src/main/java/com/feverdunk/site/service/ManagerService.java
@@ -1,10 +1,12 @@
 package com.feverdunk.site.service;
 
+import com.feverdunk.site.exceptions.ObjectNotFoundException;
 import com.feverdunk.site.models.Manager;
 import com.feverdunk.site.repository.ManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ManagerService {
@@ -16,5 +18,34 @@ public class ManagerService {
 
     public List<Manager> getManager(){
         return managerRepository.findAll();
+    }
+
+    public Manager findById(Long id) {
+        Optional<Manager> manager = managerRepository.findById(id);
+
+        return manager.orElseThrow(() -> new ObjectNotFoundException("Manager com id: {" + id + "} não foi encontrado"));
+    }
+
+    public Manager create(Manager manager) {
+        manager.setId(null);
+
+        return managerRepository.save(manager);
+    }
+
+    public Manager update(Manager managerNovo) {
+        Manager manager = findById(managerNovo.getId());
+
+        manager.setDinheiro(managerNovo.getDinheiro());
+        manager.setEmail(managerNovo.getEmail());
+        manager.setNome(managerNovo.getNome());
+        manager.setSenha(managerNovo.getSenha());
+        manager.setPremium(managerNovo.isPremium());
+        manager.setTime(managerNovo.getTime());
+
+        return managerRepository.save(manager);
+    }
+
+    public void delete(Long id){
+        managerRepository.delete(findById(id));
     }
 }
