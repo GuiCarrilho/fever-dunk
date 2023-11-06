53670aaa0cea1e468d4c3b17091093eff8902c36
Celio Camara <celiofcjunior_cg@hotmail.com>
2023-11-06 01:59:39 -0300

    Enchance GlobalExceptionHandler

--- src/main/java/com/feverdunk/site/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/feverdunk/site/exceptions/GlobalExceptionHandler.java
@@ -3,15 +3,15 @@ package com.feverdunk.site.exceptions;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.*;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.validation.BindingResult;
+
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,13 +19,14 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.IOException;
-import java.util.Objects;
 
-
+@Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler implements AuthenticationFailureHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllUncaughtExceptions(Exception ex, WebRequest request){
         final String message = "Unknow Server Error.";
+        log.error(message, ex);
         ProblemDetail body = createProblemDetail(ex, HttpStatus.INTERNAL_SERVER_ERROR,
                 message, null, null, request);
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
@@ -47,10 +48,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler imple
             sb.append("Desconhecido");
         }
         final String message = sb.toString();
+        log.error(message, ex);
         ProblemDetail body = createProblemDetail(ex, HttpStatus.BAD_REQUEST,
                 message, null, null, request);
         return handleExceptionInternal(ex, body, headers, status, request);
     }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException exception,
+                                                                        WebRequest request){
+        final String message = exception.getMostSpecificCause().getMessage();
+        log.error(message, exception);
+        ProblemDetail body = createProblemDetail(exception, HttpStatus.BAD_REQUEST, message,
+                null, null, request);
+        return handleExceptionInternal(exception, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
