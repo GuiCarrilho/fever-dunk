2d7064705ab405f642754a34c71ad3743ec90716
Celio Camara <celiofcjunior_cg@hotmail.com>
2023-11-06 18:25:09 -0300

    Create Time DTOs

---
+++ src/main/java/com/feverdunk/site/dto/time/TimeOutDTO.java
@@ -0,0 +1,20 @@
+package com.feverdunk.site.dto.time;
+
+import com.feverdunk.site.models.Contrato;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimeOutDTO {
+    private Long id;
+    private String nome;
+    private Integer pontuacao;
+    private List<Contrato> contratos;
+}
