bbbc7497686f71f596d10e8f508cca56e82def58
GabsGullo <gabrielgullo30@gmail.com>
2023-11-06 01:17:50 -0300

    Finished CRUD for Contrado

--- src/main/java/com/feverdunk/site/controller/ContratoController.java
+++ src/main/java/com/feverdunk/site/controller/ContratoController.java
@@ -1,14 +1,18 @@
 package com.feverdunk.site.controller;
 
+import com.feverdunk.site.compositeIDs.ContratoId;
+import com.feverdunk.site.exceptions.ObjectNotFoundException;
 import com.feverdunk.site.models.Contrato;
 import com.feverdunk.site.service.ContratoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contrato")
@@ -22,5 +26,44 @@ public class ContratoController {
     @GetMapping
     public List<Contrato> getContrato(){
         return contratoService.getContrato();
+    }
+
+    @GetMapping("/{id}")
+    public Contrato getContratoById(@PathVariable ContratoId id){
+        return contratoService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Contrato> post(@RequestBody @Validated Contrato contrato){
+        return criarContrato(contrato);
+    }
+
+    @PutMapping
+    public ResponseEntity<Contrato> put(@RequestBody @Validated Contrato contrato){
+        try{
+            getContratoById(contrato.getId());
+
+            Contrato contratoAtualizado = contratoService.update(contrato);
+            return ResponseEntity.ok(contratoAtualizado);
+        }catch (ObjectNotFoundException e){
+
+            return criarContrato(contrato);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Contrato> delete(@PathVariable ContratoId id){
+        contratoService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    private ResponseEntity<Contrato> criarContrato(Contrato contrato) {
+        Contrato contratoCriada = contratoService.create(contrato);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("{/id}").buildAndExpand(contratoCriada.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
