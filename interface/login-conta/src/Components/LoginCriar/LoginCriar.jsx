846c185e7b1c80850f8a5e647e9ea266581ec561
Rick Porto <rickmarco4@hotmail.com>
2023-11-07 01:00:22 -0300

    Integrating backend with front

--- interface/login-conta/src/Components/LoginCriar/LoginCriar.jsx
+++ interface/login-conta/src/Components/LoginCriar/LoginCriar.jsx
@@ -1,12 +1,53 @@
 import React, { useState } from 'react'
 import './LoginCriar.css'
-
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
 export const LoginCriar = () => {
 
   const [action, setAction] = useState("Registrar");
+
+  const handleLogin = async () => {
+    // Chama a função de login
+    await login();
+  };
+
+  async function login() {
+    let username = document.getElementById("email").value;
+    let password = document.getElementById("senha").value;
+  
+    console.log(username, password);
+  
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json; charset=utf8",
+        Accept: "application/json",
+      }),
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+  
+    let key = "Authorization";
+    let token = response.headers.get(key);
+    window.localStorage.setItem(key, token);
+  
+    if (response.ok) {
+      showToast("#okToast");
+    } else {
+      showToast("#errorToast");
+    }
+  
+    window.setTimeout(function () {
+      window.location = "/view/index.html";
+    }, 2000);
+  }
+  
+  function showToast(id) {
+    var toastElList = [].slice.call(document.querySelectorAll(id));
+  }
 
   return (
     <div className='container'>
@@ -17,19 +58,19 @@ export const LoginCriar = () => {
       <div className="inputs">
         <div className="input">
           <img src={email_icon} alt="" />
-          <input type="email" placeholder="E-mail" id="" />
+          <input type="email" placeholder="E-mail" id="email" />
         </div>
         <div className="input">
           <img src={password_icon} alt="" />
-          <input type="senha" placeholder="Senha" id="" />
+          <input type="password" placeholder="Senha" id="senha" />
         </div>
         <div className="enviar-container">
             <div className={action==="Login"?"enviar cinza":"enviar"} 
             onClick={()=>{setAction("Registrar")}}>Registrar</div>
             <div className={action==="Registrar"?"enviar cinza":"enviar"}
-            onClick={()=>{setAction("Login")}}>Login</div>
+            onClick={handleLogin}>Login</div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
