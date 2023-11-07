efc6458c7435ccc87e2c0c3121da6b63553451f0
Celio Camara <celiofcjunior_cg@hotmail.com>
2023-11-07 02:18:36 -0300

    Revert "teste"
    This reverts commit 10ad4f219e4652f21b7e7dfca3863117e3dd75b9.

--- src/main/java/com/feverdunk/site/configs/WebConfig.java
+++ src/main/java/com/feverdunk/site/configs/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "TRACE", "CONNECT");
     }
 
