adc7b1e0e77b791b3794515d858ee97c62273e70
GuiCarrilho <guicarrilho2001@gmail.com>
2023-11-09 01:36:10 -0300

    Created contrato package in dto and created ContratoInDTO

---
+++ src/main/java/com/feverdunk/site/dto/contrato/ContratoInDTO.java
@@ -0,0 +1,16 @@
+package com.feverdunk.site.dto.contrato;
+
+import com.feverdunk.site.models.compositeIDs.ContratoId;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContratoInDTO {
+
+    private ContratoId id;
+}
