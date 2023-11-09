7e2694ba3b583974cbf2c09b353d77074382c269
GuiCarrilho <guicarrilho2001@gmail.com>
2023-11-09 02:50:03 -0300

    Created query in ContratoRepository to return contratos from a specific time where vendidoEm == null

--- src/main/java/com/feverdunk/site/repository/ContratoRepository.java
+++ src/main/java/com/feverdunk/site/repository/ContratoRepository.java
@@ -3,9 +3,12 @@ package com.feverdunk.site.repository;
 import com.feverdunk.site.models.compositeIDs.ContratoId;
 import com.feverdunk.site.models.Contrato;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ContratoRepository extends JpaRepository<Contrato, ContratoId> {
+    @Query(value = "SELECT c.* FROM contratos c +" +
+            "where c.vendido_em = NULL", nativeQuery = true)
     public List<Contrato> findAllByTimeId(Long id);
 }
