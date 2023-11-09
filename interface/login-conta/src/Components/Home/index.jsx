1fd39d871e063967da013e11bcbff6aaacac6ff0
Rick Porto <rickmarco4@hotmail.com>
2023-11-09 01:51:43 -0300

    Creating Game Interface, sell, buy and money

--- interface/login-conta/src/Components/Home/index.jsx
+++ interface/login-conta/src/Components/Home/index.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
-export const Index = () => {
+const Index = () => {
+  // Lista de jogadores disponíveis para compra
+  const [availablePlayers, setAvailablePlayers] = useState([
+    { id: 1, name: 'Jogador A', price: 100 },
+    { id: 2, name: 'Jogador B', price: 150 },
+    { id: 3, name: 'Jogador C', price: 120 },
+    // Adicione mais jogadores disponíveis aqui
+  ]);
+
+  // Lista de jogadores no time
+  const [teamPlayers, setTeamPlayers] = useState([]);
+
+  // Dinheiro disponível
+  const [money, setMoney] = useState(1000);
+
   return (
-    <div className="index-container">
-      <h1>Bem-vindo à Página Index</h1>
-      <p>ENTREI NA PÁGINA</p>
+    <div>
+      <h1>Monte Seu Time de Basquete</h1>
+      <div>
+        <h2>Dinheiro Disponível: ${money}</h2>
+      </div>
+      <div>
+        <h3>Jogadores Disponíveis</h3>
+        <ul>
+          {availablePlayers.map((player) => (
+            <li key={player.id}>
+              {player.name} - ${player.price}
+              <button>Comprar</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>Seu Time</h3>
+        <ul>
+          {teamPlayers.map((player) => (
+            <li key={player.id}>
+              {player.name} - ${player.price}
+              <button>Vender</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- interface/login-conta/src/Components/Routes/roots.jsx
+++ interface/login-conta/src/Components/Routes/roots.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Index from '../Home';
+import Index from '../Home/index';
 import { LoginCriar } from '../LoginCriar/LoginCriar'
 
 const roots = () => {
